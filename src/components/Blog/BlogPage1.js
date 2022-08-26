df657b6b2e48783719e0a27bc2e05a37cc31a4e4
Ujwala <ujwalakuralkar5@gmail.com>
2022-08-26 17:37:51 +0530

    start working on blog page

---
+++ src/components/Blog/BlogPage1.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function BlogPage(){
+    return (
+        <div>
+            <h1> What is a Stop-Limit Order?</h1>
+            <p>Published Dec 9, 2018 <span>Updated Jun 29, 2022</span> </p>
+
+            <div className="Box1">
+
+            </div>
+        </div>
+       
+        
+    );
+}
+export default BlogPage;
+
